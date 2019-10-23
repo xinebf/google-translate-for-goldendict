1480caaf747f9922d9f275629e28cf65388dd401
xinebf <zhxz@cadavez.xyz>
2019-10-23 14:04:27 +0800

    update

--- googletranslate.py
+++ googletranslate.py
@@ -26,16 +26,15 @@ def get_synonym(result, resp):
         result += '\n=========\n'
         result += '0_0: Translations of {}\n'.format(query_string)
         for x in resp[1]:
-            result += '{}.\n'.format(x[0][0])
-            for i in x[2]:
-                result += '{}: {}\n'.format(i[0], ", ".join(i[1]))
+            result += '# {}.\n'.format(x[0][0])
+            for y in x[2]:
+                result += '{}: {}\n'.format(y[0], ", ".join(y[1]))
     return result
 
 
 def get_result(results, resp):
     for x in resp[0]:
-        if x[0]:
-            results += x[0]
+        results += x[0] if x[0] else ''
     return results
 
 
@@ -43,7 +42,7 @@ def get_definitions(result, resp):
     result += '\n=========\n'
     result += '0_0: Definitions of {}\n'.format(query_string)
     for x in resp[12]:
-        result += '{}.\n'.format(x[0])
+        result += '# {}.\n'.format(x[0])
         for y in x[1]:
             result += '  - {}\n'.format(y[0])
             result += '    * {}\n'.format(y[2]) if len(y) >= 3 else ''
@@ -61,10 +60,10 @@ def get_examples(result, resp):
 def get_synonyms_en(result, resp):
     result += '\n=========\n'
     result += '0_0: Synonyms of {}\n'.format(query_string)
-    for i in resp[11]:
-        result += "{}.\n---\n".format(i[0])
-        for j in i[1]:
-            result += ", ".join(j[0]) + "\n"
+    for x in resp[11]:
+        result += "{}.\n---\n".format(x[0])
+        for y in x[1]:
+            result += ", ".join(y[0]) + "\n"
         result += "\n******\n"
     return result
 
@@ -85,14 +84,12 @@ def get_translation():
             result += '^_^: Translate {} To {}\n'.format(resp[2], alternative_language)
             url = get_url(alternative_language, parse_query)
             resp_en = session.get(url, timeout=3).json()
-            result = get_result(result, resp_en)
-            result += '\n'
-        else:
-            result += '^_^: Translate {} To {}\n{}\n'.format(resp[2], target_language, query_string)
-        result = get_result(result, resp)
-        if resp[2] == target_language:
+            result = get_result(result, resp_en) + '\n'
+            result = get_result(result, resp)
             result = get_synonym(result, resp_en)
         else:
+            result += '^_^: Translate {} To {}\n{}\n'.format(resp[2], target_language, query_string)
+            result = get_result(result, resp)
             result = get_synonym(result, resp)
         if synonyms_en and len(resp) >= 12 and resp[11]:
             result = get_synonyms_en(result, resp)
@@ -103,6 +100,8 @@ def get_translation():
         print(result.encode(result_code, 'ignore').decode(result_code))
     except requests.exceptions.ReadTimeout:
         print('╰（‵□′）╯: ReadTimeout...')
+    except:
+        print('Errrrrrrrrror')
 
 
 if __name__ == "__main__":
