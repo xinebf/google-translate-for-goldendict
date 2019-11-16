218e9d1b1c5d7e7a8eccb4ac5679344332f0f0e1
xinebf <zhxz@cadavez.xyz>
2019-11-16 21:27:50 +0800

    change to f-Strings

--- googletranslate.py
+++ googletranslate.py
@@ -33,18 +33,18 @@ class GoogleTranslate(object):
         self.result = ''
 
     def get_url(self, tl, qry):
-        url = 'https://{}/translate_a/single?client=gtx&sl=auto&tl={}&dt=at&dt=bd&dt=ex&' \
-               'dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&q={}'.format(self.http_host, tl, qry)
+        url = f'https://{self.http_host}/translate_a/single?client=gtx&sl=auto&tl={tl}&dt=at&dt=bd&dt=ex&' \
+              f'dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&q={qry}'
         return url
 
     def get_synonym(self, resp):
         if resp[1]:
             self.result += '\n=========\n'
-            self.result += '0_0: Translations of {}\n'.format(self.query_string)
+            self.result += f'0_0: Translations of {self.query_string}\n'
             for x in resp[1]:
-                self.result += '# {}.\n'.format(x[0][0])
+                self.result += f'# {x[0][0]}.\n'
                 for y in x[2]:
-                    self.result += '{}: {}\n'.format(y[0], ', '.join(y[1]))
+                    self.result += f'{y[0]}: {", ".join(y[1])}\n'
 
     def get_result(self, resp):
         for x in resp[0]:
@@ -53,32 +53,32 @@ class GoogleTranslate(object):
 
     def get_definitions(self, resp):
         self.result += '\n=========\n'
-        self.result += '0_0: Definitions of {}\n'.format(self.query_string)
+        self.result += f'0_0: Definitions of {self.query_string}\n'
         for x in resp[12]:
-            self.result += '# {}.\n'.format(x[0]) if x[0] else ''
+            self.result += f'# {x[0] if x[0] else ""}.\n'
             for y in x[1]:
-                self.result += '  - {}\n'.format(y[0])
-                self.result += '    * {}\n'.format(y[2]) if len(y) >= 3 else ''
+                self.result += f'  - {y[0]}\n'
+                self.result += f'    * {y[2] if len(y) >= 3 else ""}\n'
 
     def get_examples(self, resp):
         self.result += '\n=========\n'
-        self.result += '0_0: Examples of {}\n'.format(self.query_string)
+        self.result += f'0_0: Examples of {self.query_string}\n'
         for x in resp[13][0]:
-            self.result += '  - {}\n'.format(x[0].replace('<b>', '').replace('</b>', ''))
+            self.result += f'  - {x[0].replace("<b>", "").replace("</b>", "")}\n'
 
     def get_synonyms_en(self, resp):
         self.result += '\n=========\n'
-        self.result += '0_0: Synonyms of {}\n'.format(self.query_string)
+        self.result += f'0_0: Synonyms of {self.query_string}\n'
         for x in resp[11]:
-            self.result += '{}.\n---\n'.format(x[0])
+            self.result += f'{x[0]}.\n---\n'
             for y in x[1]:
                 self.result += ', '.join(y[0]) + '\n'
             self.result += '\n******\n'
 
     def get_resp(self, url):
         proxies = {
-            'http': 'http://{}'.format(self.http_proxy.strip() if self.http_proxy.strip() else '127.0.0.1:1080'),
-            'https': 'http://{}'.format(self.http_proxy.strip() if self.http_proxy.strip() else '127.0.0.1:1080')
+            'http': f'http://{self.http_proxy.strip() if self.http_proxy.strip() else "127.0.0.1:1080"}',
+            'https': f'http://{self.http_proxy.strip() if self.http_proxy.strip() else "127.0.0.1:1080"}'
         }
         base_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'}
         session = requests.Session()
@@ -99,12 +99,12 @@ class GoogleTranslate(object):
             resp_alt = loop.run_in_executor(None, partial(self.get_resp, url_alt))
             [resp, resp_alt] = await asyncio.gather(resp, resp_alt)
             if resp[2] == self.target_language:
-                self.result += '^_^: Translate {} To {}\n'.format(resp[2], self.alternative_language)
+                self.result += f'^_^: Translate {resp[2]} To {self.alternative_language}\n'
                 self.get_result(resp_alt)
                 self.get_result(resp)
                 self.get_synonym(resp_alt)
             else:
-                self.result += '^_^: Translate {} To {}\n{}\n'.format(resp[2], self.target_language, self.query_string)
+                self.result += f'^_^: Translate {resp[2]} To {self.target_language}\n{self.query_string}\n'
                 self.get_result(resp)
                 self.get_synonym(resp)
             if self.synonyms_en and len(resp) >= 12 and resp[11]:
