d78965c0909dfe3be52c06c9cf5447aab7fa55c7
xinebf <zhxz@cadavez.xyz>
2020-01-09 21:19:30 +0800

    update: googletranslate.ui

--- googletranslate.ui.py
+++ googletranslate.ui.py
@@ -7,6 +7,8 @@ import asyncio
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
+import threading as td
+from queue import Queue
 
 from googletranslate import GoogleTranslate
 
@@ -29,17 +31,26 @@ class UITranslate(GoogleTranslate):
         self.input_entry = ttk.Entry(frm_input, textvariable=self.input_text)
         self.input_entry.pack(side=tk.LEFT, fill=tk.X, expand=1)
 
-        self.trans_button = ttk.Button(frm_input, text='trans', width=6, command=self.trans)
+        self.run_queue = Queue()
+        self.trans_button = ttk.Button(frm_input, text='trans', width=6, command=self.run)
         self.trans_button.pack(anchor=tk.NE, side=tk.LEFT)
 
         self.st = scrolledtext.ScrolledText(frm_output, state=tk.NORMAL)
         self.st.insert(tk.END, 'Google Translate\n')
         self.st.pack(anchor=tk.NW, fill=tk.BOTH, expand=1)
 
-        self.input_entry.bind('<Return>', self.trans)
+        self.input_entry.bind('<Return>', self.run)
         self.input_entry.bind('<Escape>', lambda event=None: self.input_text.set(''))
 
-    def trans(self, event=None):
+    def run(self, event=None):
+        if self.run_queue.empty():
+            td.Thread(target=self.trans).start()
+
+    def trans(self):
+        gui_style = ttk.Style()
+        gui_style.configure('Ui.TButton', foreground='#FF0000')
+        self.trans_button.config(style='Ui.TButton')
+        self.run_queue.put(1)
         if self.input_text.get():
             query_string = self.input_text.get()
         else:
@@ -51,6 +62,8 @@ class UITranslate(GoogleTranslate):
         asyncio.run(self.get_translation(target_language='zh-CN', query_string=query_string))
         self.st.delete('1.0', tk.END)
         self.st.insert(tk.END, self.result)
+        self.run_queue.get()
+        gui_style.configure('Ui.TButton', foreground='#000000')
 
 
 if __name__ == '__main__':
